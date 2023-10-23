07d802451592a800c657682afe8512288b98d914
Augusto Pires <91226967+augustop01@users.noreply.github.com>
2023-10-23 18:48:01 -0300

    Resolução do exercício 2

---
+++ src/m1s03/ex_02/Contador.java
@@ -0,0 +1,11 @@
+package m1s03.ex_02;
+
+public class Contador {
+    public static void contarPalavras(String frase){
+
+        String[] separador = frase.trim().split("\\s+");
+        Integer contador = separador.length;
+
+        System.out.printf("A frase %s '%s' %s é composta por: %d palavras.", "\u001B[33m", frase, "\u001B[0m", contador);
+    }
+}
---
+++ src/m1s03/ex_02/TesteContador.java
@@ -0,0 +1,12 @@
+package m1s03.ex_02;
+
+import java.util.Scanner;
+
+public class TesteContador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite uma frase para que o programa conte a quantidade de palavras:");
+        String frase = scanner.nextLine();
+        Contador.contarPalavras(frase);
+    }
+}
