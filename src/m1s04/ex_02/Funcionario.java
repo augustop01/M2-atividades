69d2e5794b175fe2b4a8403f8bc7e86466e36f9d
Augusto Pires <91226967+augustop01@users.noreply.github.com>
2023-11-09 17:29:52 -0300

    Resolução exercício 2

---
+++ src/m1s04/ex_02/Funcionario.java
@@ -0,0 +1,7 @@
+package m1s04.ex_02;
+
+public class Funcionario {
+    public String obterCargo(){
+        return "Sou Funcionário.";
+    }
+}
---
+++ src/m1s04/ex_02/Gerente.java
@@ -0,0 +1,8 @@
+package m1s04.ex_02;
+
+public final class Gerente extends Funcionario{
+    @Override
+    public String obterCargo(){
+        return "Sou Gerente.";
+    }
+}
